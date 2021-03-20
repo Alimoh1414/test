984368154f524131a6aa866c2d8a1b849713f93d
ALI MOHAMMAD <46354274+Alimoh1414@users.noreply.github.com>
2021-03-20 22:16:02 +0300

    Update index.dart

--- file1/index.dart
+++ file1/index.dart
@@ -1,20 +1,21 @@
 import 'dart:math';
+
 void main() {
-
   Random random = Random();
-
   List<int> myList = [];
 
-  for ( int i = 0 ; i < 20 ; i++ ){
-    int myInt = random.nextInt(500);
-    myList.add(myInt);
-  }
+  print(myList.length);
 
-  for ( int e in myList ){
-    if(e % 2 == 0 ){
-      print("$e is Double");
-    }else{
-      print('$e is single');
+  while (myList.length < 100) {
+    int myInt = random.nextInt(100);
+    if (!myList.contains(myInt)) {
+      myList.add(myInt);
     }
   }
+  var myRange = myList.getRange(25, 50);
+
+  for (var e in myRange) {
+    print(e);
+  }
 }
+
