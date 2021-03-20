2fb927bd4cc9c59efdd6d4c65be504f666e5a333
ALI MOHAMMAD <46354274+Alimoh1414@users.noreply.github.com>
2021-03-20 22:33:03 +0300

    Update index.dart

--- file1/index.dart
+++ file1/index.dart
@@ -6,27 +6,13 @@ void main() {
 
   print(myList.length);
 
-  while (myList.length < 50) {
-    int myInt = random.nextInt(50);
+  while (myList.length < 150) {
+    int myInt = random.nextInt(500);
     if (!myList.contains(myInt)) {
       myList.add(myInt);
     }
   }
+
   print(myList.length);
   print(myList);
-
-  List<int> evenNumbers = [];
-  List<int> oddNumbers = [];
-
-  for (int e in myList) {
-    if (e % 2 == 0) {
-      evenNumbers.add(e);
-    } else {
-      oddNumbers.add(e);
-    }
-  }
-  print(evenNumbers);
-  print(evenNumbers.length);
-  print(oddNumbers);
-  print(oddNumbers.length);
 }
